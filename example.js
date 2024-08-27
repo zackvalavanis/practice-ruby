73cfdc685e51131bb0edcaa0328b461cfb479bcf
Zack <168221270+Zval321@users.noreply.github.com>
2024-08-27 15:56:42 -0500

    JS

---
+++ example.js
@@ -0,0 +1,15 @@
+// Write a Playlist class that stores a name and an array of songs with methods to add a song, remove a song, shuffle the songs into a random order, and display all the songs.
+
+class Playlist {
+  constructor(name, songs) { 
+    this.name = name;
+    this.songs = songs;
+  }
+  add_song(song) { 
+    this.songs.push(song);
+  }
+  remove_song(song) {
+    this.songs.delete(song);
+  }
+}
+   

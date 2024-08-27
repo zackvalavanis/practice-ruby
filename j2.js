92edc0f0033d360ce2c8f5617c08cc171e016b17
Zack <168221270+Zval321@users.noreply.github.com>
2024-08-27 16:12:33 -0500

    Deliberate Practice 2

---
+++ j2.js
@@ -0,0 +1,4 @@
+// Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+var word = 'hello'
+console.log(word.toUpperCase())
---
+++ r2.rb
@@ -0,0 +1,4 @@
+# Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+word = 'hello'
+pp word.upcase

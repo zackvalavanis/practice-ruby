16135973f3f9ff8399f62335cc6d7e0151ae75c1
Zack <168221270+Zval321@users.noreply.github.com>
2024-08-27 16:07:10 -0500

    Deliberate_practice

--- example.js
+++ example.js
@@ -12,4 +12,3 @@ class Playlist {
     this.songs.delete(song);
   }
 }
-   
---
+++ j1.js
@@ -0,0 +1,4 @@
+// Write a program that uses a variable to store a number, then prints that number plus 10.
+
+var number = 10
+console.log(number + 10)

0d9afd355b060526b5bfe1d5222446123c37231a
Zack <168221270+Zval321@users.noreply.github.com>
2024-08-28 10:13:15 -0500

    More Ruby and JS Practice

---
+++ j5.js
@@ -0,0 +1,3 @@
+// Write a program that uses a variable to store a number, then prints the number times 10.
+var number = 10 
+console.log(number * 10)
---
+++ r5.rb
@@ -0,0 +1,5 @@
+# Write a program that uses a variable to store a number, then prints the number times 10.
+
+number = 10 
+
+puts number * 10

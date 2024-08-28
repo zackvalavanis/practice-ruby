bd4fdbdae03b28d04a54ff7d94842f620c35dd9b
Zack <168221270+Zval321@users.noreply.github.com>
2024-08-28 10:00:46 -0500

    Deliberate Practice JS & Ruby

---
+++ j4.js
@@ -0,0 +1,5 @@
+// Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+var word = 'olleh'
+
+console.log(word.split("").reverse().join(""))
---
+++ r4.rb
@@ -0,0 +1,4 @@
+# Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+word = 'olleh'
+pp word.reverse

765e8e227853423af7dd633cab606cce9ea73927
Zack <168221270+Zval321@users.noreply.github.com>
2024-08-28 14:25:11 -0500

    ruby & JS Practice

---
+++ j13.js
@@ -0,0 +1,9 @@
+// Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+var number = 10
+
+if(number == 10) { 
+  console.log(0)
+} else { 
+  console.log(-1)
+}
---
+++ rb13.rb
@@ -0,0 +1,9 @@
+# Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+number = 1
+
+if number == 10 
+  puts 0 
+else puts -1
+end 
+

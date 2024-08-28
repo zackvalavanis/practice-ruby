6b4f1508ab485a59f9e221e3b1adbdb1182eb472
Zack <168221270+Zval321@users.noreply.github.com>
2024-08-28 09:45:22 -0500

    Ruby & JS Practice

---
+++ j3.js
@@ -0,0 +1,7 @@
+// Write a program that uses variables to store two numbers, then prints the numbers added together.
+
+var num1 = 1
+var num2 = 2
+
+var sum = num1 + num2
+console.log(sum)
---
+++ r3.rb
@@ -0,0 +1,7 @@
+# Write a program that uses variables to store two numbers, then prints the numbers added together.
+
+number1 = 1
+number2 = 2
+
+sum = number1 + number2 
+pp sum

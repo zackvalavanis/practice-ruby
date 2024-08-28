6c0413847c800912880e9f0eaf7c67aaca259d5f
Zack <168221270+Zval321@users.noreply.github.com>
2024-08-28 11:18:03 -0500

    js and ruby practice

---
+++ j10.js
@@ -0,0 +1,7 @@
+// Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+  var word = window.prompt("Please enter a word:");
+  
+if (word === 'marco') {
+    console.log('polo');
+  }
---
+++ r10.rb
@@ -0,0 +1,8 @@
+# Write a program that asks the user to input a word. If the word is "marco", print "polo".
+puts "Please enter an word"
+word = gets.chomp
+
+if word == 'marco' || word == 'Marco'
+  puts 'polo' 
+end 
+

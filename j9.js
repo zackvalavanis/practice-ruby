9e8c5e141ce73fa618022b024b0b0cc6c500bbee
Zack <168221270+Zval321@users.noreply.github.com>
2024-08-28 11:07:56 -0500

    Ruby and Js Practice

---
+++ j9.js
@@ -0,0 +1,6 @@
+// Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+var first_name = 'Zack';
+var last_name = 'Valavanis';
+var both = `My name is ${first_name} ${last_name}!`
+console.log(both)

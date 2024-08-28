bb2cc2cec054b09fcfb4ebd003c47761ae8af8eb
Zack <168221270+Zval321@users.noreply.github.com>
2024-08-28 10:54:10 -0500

    More Ruby & JS Practice

---
+++ j8.js
@@ -0,0 +1,8 @@
+// Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+var first_name = 'zack'
+var last_name = "valavanis"
+
+var  both = first_name + " " + last_name
+console.log(both)
+
---
+++ r8.rb
@@ -0,0 +1,6 @@
+# Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+first_name = 'Zack'
+last_name = 'Valavanis'
+both = first_name + " " + last_name 
+puts both  

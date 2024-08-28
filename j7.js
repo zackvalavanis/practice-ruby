c7a89fbcfb399b021dd0883802886ca5da960218
Zack <168221270+Zval321@users.noreply.github.com>
2024-08-28 10:37:54 -0500

    Practice Ruby & JS

---
+++ j7.js
@@ -0,0 +1,5 @@
+// Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+
+var number = 1.06;
+console.log(parseInt(number));
+
---
+++ r7.rb
@@ -0,0 +1,4 @@
+# Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+
+number = 1.06
+puts number.to_i

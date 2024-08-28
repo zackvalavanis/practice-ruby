f1726402c9d73b3da3d04660c233c63a794b84e5
Zack <168221270+Zval321@users.noreply.github.com>
2024-08-28 12:02:11 -0500

    Average Salary: Chicago Public Jobs

--- Api_practice.rb
+++ Api_practice.rb
@@ -2,7 +2,17 @@ require 'HTTP'
 response = HTTP.get("https://data.cityofchicago.org/resource/xzkq-xp2w.json")
 data = response.parse
 
+sum_of_salaries = 0
+
 data.each do |element|
-  puts element["job_titles"]
-  puts element["annual_salary"]
+  element["job_titles"]
+  element["annual_salary"]
+  sum_of_salaries += element['annual_salary'].to_i
 end
+
+total_amount_jobs = data.count { |element| element["Job_titles"].to_i}
+
+average_salary = sum_of_salaries / total_amount_jobs
+
+pp average_salary
+

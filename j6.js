21d4047c130efa65d9b04149d1553c6732cf7d16
Zack <168221270+Zval321@users.noreply.github.com>
2024-08-28 10:25:13 -0500

    More Ruby and JS practice

---
+++ j6.js
@@ -0,0 +1,8 @@
+// Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+var word1 = 'hello'
+var word2 = 'zack'
+
+both = word1.toUpperCase() + " " + word2.toUpperCase()
+
+console.log(both)
+
---
+++ r6.rb
@@ -0,0 +1,8 @@
+# Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+word1 = 'hello'
+word2 = 'zack'
+
+both = word1.upcase + " " + word2.upcase
+
+puts both

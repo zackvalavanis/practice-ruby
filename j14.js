5f9bbd658e92fa2bc2a596fc7ac782fb9c404d72
Zack <168221270+Zval321@users.noreply.github.com>
2024-08-28 14:33:52 -0500

    ruby and js practice

---
+++ j14.js
@@ -0,0 +1,10 @@
+// Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+var num1 = 8
+var num2 = 12
+
+if(num1 < 10 && num2 < 10) { 
+  console.log(1);
+} else { 
+  console.log(0);
+}
---
+++ r14.rb
@@ -0,0 +1,10 @@
+# Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+number1 = 3
+number2 = 8
+
+if number1 < 10 && number2 < 10 
+  puts 1
+else puts 0 
+end 
+

df84a3d9d5d53fbfb9561f846e290e94a2884efb
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-20 13:13:05 -0500

    js practice

---
+++ j25.js
@@ -0,0 +1,9 @@
+// Start with an array of strings and create a new array with each string upcased.
+//For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
+
+var strings = ['hello', 'goodbye'];
+
+new_array = strings.map( (word) => word.toUpperCase());
+
+console.log(new_array);
+

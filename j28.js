bfa4bed26157c5e262293d7f9f4d2220336b489f
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-21 14:41:50 -0500

    js practice filter method

---
+++ j28.js
@@ -0,0 +1,11 @@
+// Start with an array of numbers and create a new array with only the odd numbers.
+// For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
+
+
+var numbers = [2, 4, 5, 1, 8, 9, 7];
+
+
+new_array = numbers.filter( (number) => number % 2 != 0);
+
+console.log(new_array);
+

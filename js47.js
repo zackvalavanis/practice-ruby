cc58a64cec2cd888c3002faabb15e2006597035b
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-27 10:37:31 -0500

    js practice

---
+++ js47.js
@@ -0,0 +1,10 @@
+// Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
+
+var hash = {
+  word: {name: 'chris'}, 
+  word2: {name: 'bobby'}
+}
+
+hash.word3 = {name: 'boots'}
+
+console.log(hash);
